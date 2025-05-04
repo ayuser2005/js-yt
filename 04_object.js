b2d0b504d58ec5a15ce498869f3f73b6cc2f3a1e
ayuser2005 <anand23154040@akgec.ac.in>
2025-05-04 02:02:07 +0000

    json

---
+++ 04_object.js
@@ -0,0 +1,25 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+const{courseInstructor}=course
+console.log(courseInstructor);
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
