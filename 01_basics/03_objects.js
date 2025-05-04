aa60f164b65e7678dd11abd4eb47f2a4646e6e5c
ayuser2005 <anand23154040@akgec.ac.in>
2025-05-04 02:38:03 +0000

    objects

---
+++ 01_basics/03_objects.js
@@ -0,0 +1,12 @@
+//object literals
+const JsUser={
+    name:"Anand",
+    age:"19",
+    location:"Jaipur",
+    email:"anand@google.com",
+    isLoggedin: false,
+    lastLoginDays:["Monday","Saturday"]
+
+}
+console.log(JsUser.email)
+console.log(JsUser["email"])
